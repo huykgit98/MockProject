5ed9daacfc24de9dc5eb247b19c19df9bc0af645
huy nguyen <huykgit98@gmail.com>
2019-10-28 20:48:09 +0700

    add client view

--- src/main/java/edu/sgu/bookingsystem/dao/UserDao.java
+++ src/main/java/edu/sgu/bookingsystem/dao/UserDao.java
@@ -1,5 +1,12 @@
 package edu.sgu.bookingsystem.dao;
 
-public interface UserDao {
+import edu.sgu.bookingsystem.model.Employee;
+import edu.sgu.bookingsystem.model.User;
 
+public interface UserDao {
+	Employee getEmployeeByAccount(User account);
+	
+	User getUserNameByUserName(User account);
+	
+	User getUserNameByUserName(User account, String usernameSave);
 }
--- src/main/java/edu/sgu/bookingsystem/dao/impl/UserDaoImpl.java
+++ src/main/java/edu/sgu/bookingsystem/dao/impl/UserDaoImpl.java
@@ -1,7 +1,27 @@
 package edu.sgu.bookingsystem.dao.impl;
 
 import edu.sgu.bookingsystem.dao.UserDao;
+import edu.sgu.bookingsystem.model.Employee;
+import edu.sgu.bookingsystem.model.User;
 
 public class UserDaoImpl implements UserDao {
+
+	@Override
+	public Employee getEmployeeByAccount(User account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserNameByUserName(User account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserNameByUserName(User account, String usernameSave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
